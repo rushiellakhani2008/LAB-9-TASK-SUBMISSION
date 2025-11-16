41409969e5d18d26db2319adc033061eec8de2db
Rushiel Lakhani <K253008@nu.edu.pk>
2025-11-16 16:34:13 +0500

    Update Task 1.c

--- Task 1.c
+++ Task 1.c
@@ -1,39 +1,60 @@
-#include <stdio.h>
-void bed(int beds[3][5]){
-    int count =0;
-    int i,j;
-   
-   
-for (i=0; i<3; i++){
-    if(i == 0){
-      printf("Enter the bed status for beds in General ward \n:");
-    }
-    else if(i == 1){
-      printf("Enter the bed status for beds in ICU ward \n:");
-    }
-    else if(i == 2){
-      printf("Enter the bed status for beds in Private ward \n:");
-    }
-   
-   
-for (j=0; j<5; j++){
-printf("Bed [%d][%d] :",i ,j);
-scanf("%d", &beds[i][j]);
-if(beds[i][j] == 1){
-   count++;
-}
-}
-}
-int counter = 15 - count;
-printf("%d Beds are occupied while %d Beds are available.",count , counter);
+#include<stdio.h>
 
+#define WARDS 3
+#define BEDS 5
+
+void recordStatus(int arr[WARDS][BEDS]);
+void countBeds(int arr[WARDS][BEDS], int *occupied, int *empty);
+void displayStatus(int arr[WARDS][BEDS]);
+
+int main() {
+    int hospital[WARDS][BEDS];
+    int occupied = 0, empty = 0;
+
+    recordStatus(hospital);
+    countBeds(hospital, &occupied, &empty);
+    displayStatus(hospital);
+
+    printf("\nTotal occupied beds: %d\n", occupied);
+    printf("Total available beds: %d\n", empty);
+
+    return 0;
 }
 
-int main(){
-    int beds[3][5];
-          printf("Enter the bed status (1 for occupied, 0 for empty)\n");
-     bed(beds);
- 
-return 0;
+void recordStatus(int arr[WARDS][BEDS]) {
+	int i,j;
+    printf("\nEnter bed status (1 = occupied, 0 = empty):\n");
+    for(i = 0; i < WARDS; i++) {
+        printf("\n--- Ward %d ---\n", i + 1);
+        for(j = 0; j < BEDS; j++) {
+            printf("Bed %d: ", j + 1);
+            scanf("%d", &arr[i][j]);
+        }
+    }
 }
 
+void countBeds(int arr[WARDS][BEDS], int *occupied, int *empty){
+	int i,j;
+    for(i = 0; i < WARDS; i++) {
+        for(j = 0; j < BEDS; j++) {
+            if(arr[i][j] == 1)
+                (*occupied)++;
+            else
+                (*empty)++;
+        }
+    }
+}
+
+void displayStatus(int arr[WARDS][BEDS]) {
+	int i,j;
+    printf("\n== Bed Status Report ==\n");
+    for(i = 0; i < WARDS; i++) {
+        if(i == 0) printf("\nGeneral Ward: ");
+        else if(i == 1) printf("\nICU Ward:     ");
+        else printf("\nPrivate Ward: ");
+
+        for(j = 0; j < BEDS; j++) {
+            printf("%d ", arr[i][j]);
+        }
+    }
+}
